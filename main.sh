694d128a44711125d1a9dfaf74717efc2ddefaa6
schreck4761 <schreck4761@gmail.com>
2022-07-14 14:42:57 -0400

    Update main.sh to have gradle build it's own image

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
-./gradlew bootBuildImage --imageName=schreck/payrollservice
+./gradlew bootBuildImage --imageName=schreck/payrollservice/build
 docker build -t schreck/payrollservice .
 docker run -p 8080:8080 -t schreck/payrollservice
